cbe25960151b3d68ed95aded7c2a3f6789a5649e
Sjimenez <sj@phoenixconnection.es>
2022-07-01 14:06:45 +0200

    los partidos de distintas jornadas se muestran bien dinamicamente

--- frontend/src/components/options/MatchTable.js
+++ frontend/src/components/options/MatchTable.js
@@ -15,49 +15,87 @@ export const MatchTable = (year) => {
 		getMatches();
 	}, [year.year]);
 
-	//funcion para pintar las filas
-	const renderTableHeader = () => {
+	//render table
+	const renderTable = () => {
+		let journey = 1;
+		let arrayJourney = [];
+		let allTables = [];
+		let journeyTable;
+		let maxJourney = Math.max(...matches.map((o) => o.journey));
+		if (matches.length === 0) {
+			return <h1 className="journey_center">No hay partidos disponibles</h1>;
+		}
+		while (journey <= maxJourney) {
+			arrayJourney = matches.filter((m) => {
+				return m.journey === journey;
+			});
+
+			journeyTable = (
+				<div key={'div-journey' + journey}>
+					<h2 key={'h2-' + journey} className="journey_center">
+						Jornada {journey}
+					</h2>
+					<Table key={'table-' + journey} responsive="sm" striped>
+						{renderTableHeader(journey)}
+						{renderTableRows(journey, arrayJourney)}
+					</Table>
+				</div>
+			);
+			allTables.push(journeyTable);
+			journey++;
+		}
+
+		return allTables;
+	};
+
+	//render table header
+	const renderTableHeader = (journey) => {
+		let tableHeader = (
+			<thead key={'thead-' + journey}>
+				<tr key={'tr-' + journey}>
+					<th key={'th-partido-' + journey}>Partido</th>
+					<th key={'th-local-' + journey}>Local</th>
+					<th key={'th-visitante-' + journey}>Visitante</th>
+					<th key={'th-resultado-' + journey}>Resultado</th>
+					<th key={'th-fecha-' + journey}>Fecha</th>
+					<th key={'th-hora-' + journey}>Hora</th>
+					<th key={'th-campo-' + journey}>Campo</th>
+				</tr>
+			</thead>
+		);
+		return tableHeader;
+	};
+
+	//render rows
+	const renderTableRows = (journey, arrayJourney) => {
 		let bodyTR = [];
-		if (matches) {
-			matches.forEach((element) => {
+		let distinctRow;
+		if (arrayJourney) {
+			arrayJourney.forEach((element) => {
 				let matchDate = new Date(parseFloat(element.date)).toLocaleDateString(
 					'es'
 				);
 				let matchHour = new Date(parseFloat(element.date)).toLocaleTimeString();
+				distinctRow = journey + '-' + (bodyTR.length + 1);
 				bodyTR.push(
-					<tr key={element.date}>
-						<td>{bodyTR.length + 1}</td>
-						<td>{element.team_1.team.name}</td>
-						<td>{element.team_2.team.name}</td>
-						<td>
+					<tr key={'tr-' + distinctRow}>
+						<td key={'td-partido-' + distinctRow}>{bodyTR.length + 1}</td>
+						<td key={'td-local-' + distinctRow}>{element.team_1.team.name}</td>
+						<td key={'td-visitante-' + distinctRow}>
+							{element.team_2.team.name}
+						</td>
+						<td key={'td-resultado-' + distinctRow}>
 							{element.team_1.score}-{element.team_2.score}
 						</td>
-						<td>{matchDate}</td>
-						<td>{matchHour}</td>
-						<td>{element.place}</td>
+						<td key={'td-fecha-' + distinctRow}>{matchDate}</td>
+						<td key={'td-hora-' + distinctRow}>{matchHour}</td>
+						<td key={'td-campo-' + distinctRow}>{element.place}</td>
 					</tr>
 				);
 			});
-			return bodyTR;
+			return <tbody>{bodyTR}</tbody>;
 		}
 	};
 
-	return (
-		<>
-			<Table responsive="sm" striped>
-				<thead>
-					<tr>
-						<th>Partido</th>
-						<th>Local</th>
-						<th>Visitante</th>
-						<th>Resultado</th>
-						<th>Fecha</th>
-						<th>Hora</th>
-						<th>Campo</th>
-					</tr>
-				</thead>
-				<tbody>{renderTableHeader()}</tbody>
-			</Table>
-		</>
-	);
+	return <>{renderTable()}</>;
 };
